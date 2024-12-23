d29b9c7d2b77a0f2aecd4383412ce3dd11159de3
khushisanghani <khushisanghani2503@gmail.com>
2024-12-23 23:12:35 +0530

    practice

--- src/Components/Practice.js
+++ src/Components/Practice.js
@@ -6,10 +6,16 @@ function Practice(){
         {/* <div class="container-sm border pt-5 my-5">100% wide until small breakpoint</div> */}
         <div className='container border'>
             <div className='row text-center'>
-                <div className="col-md-3 bg-info border w-10"><h1>First</h1></div>
-                <div className="col-md-3 bg-secondary border"><h1>Second</h1></div>
-                <div className="col-md-3 bg-primary border"><h1>Third</h1></div>
-                <div className="col-md-3 bg-success border"><h1>Fourth</h1></div>
+                <div className="col-md-3 bg-info border w-10 order-4"><h1>First</h1></div>
+                <div className="col-md-3 bg-secondary border order-3"><h1>Second</h1></div>
+                <div className="col-md-3 bg-primary border order-2"><h1>Third</h1></div>
+                <div className="col-md-3 bg-success border order-1"><h1>Fourth</h1></div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row border">
+                <div class="col-md-3 bg-info">1</div>
+                <div class="col-md-3 bg-primary offset-md-6">2</div>
             </div>
         </div>
         </>
